0b36872437159551dd5d056fd025a33c717f1889
manthansharadbire <manthansharadbire@gmail.com>
2025-01-26 23:50:14 +0530

    moviecard details has been added

--- src/MovieCards.js
+++ src/MovieCards.js
@@ -1,20 +1,21 @@
 export default function TitleName(){
         return(
             <div>
-            <h1 className="heading">Movie Cards</h1>
+            <h1 className="heading">MOVIE CARDS</h1>
             </div>
         );
     }
 
 
 export function MovieCard(inputinfo){
-        const {title , year, Genre , movieinfo } = inputinfo;
+        const {title , year, Genre , info, movieimglink} = inputinfo;
         return(
             <div className="CardsContainer">
             <div className="Cards">
-                <h1>{title}</h1>
+                <h1 className="MovieTitle">{title}</h1>
                 <span className="MovieYear">{year}</span>
-                <span className="MovieGenre">{Genre}</span>
+                <span className="MovieGenre">{Genre}</span><br/><br/>
+                <span className="MovieInfo">{info}</span>
             </div>
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css'
 root.render(
     <>
     <TitleName/>
-    <MovieCard title="DEADPOOL" year="( 2016 )" Genre="Action, Adventure , Sci-fi "/>
-    <MovieCard title= "SUPERMAN" year="( 2014 )" Genre="Action, Adventure , Sci-fi , Fantasy"/>
+    <MovieCard title="DEADPOOL" year="( 2016 )" Genre="Action, Adventure , Sci-fi " info="A wisecracking mercenary gets experimented on and becomes immortal yet hideously scarred, and sets out to track down the man who ruined his looks."/>
+    <MovieCard title= "SUPERMAN" year="( 1978 )" Genre="Action, Adventure , Sci-fi , Fantasy" info="An alien orphan is sent from his dying planet to Earth, where he grows up to become his adoptive home's first and greatest superhero."/>
     </>
 );
